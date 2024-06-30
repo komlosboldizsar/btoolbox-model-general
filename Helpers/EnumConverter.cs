074045a406a2137e0433647886790e19d6aa98e2
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-06-30 13:06:29 +0200

    Add enum converters as helpers (from OpenSC)

---
+++ Helpers/EnumConverter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace BToolbox.Helpers
+{
+
+    public class EnumConverter<TEnum, TConvertedValue>: IEnumerable<KeyValuePair<TEnum, TConvertedValue>>
+    {
+
+        private readonly Dictionary<TEnum, TConvertedValue> convertedValues = new();
+        private readonly TConvertedValue defaultValue;
+
+        public EnumConverter(TConvertedValue defaultValue = default, Dictionary <TEnum, TConvertedValue> convertedValues = null)
+        {
+            this.defaultValue = defaultValue;
+            if (convertedValues != null)
+                foreach (var item in convertedValues)
+                    this.convertedValues.Add(item.Key, item.Value);
+        }
+
+        public TConvertedValue Convert(TEnum enumValue)
+        {
+            if (!convertedValues.TryGetValue(enumValue, out TConvertedValue convertedValue))
+                return defaultValue;
+            return convertedValue;
+        }
+
+        #region Implement IEnumerable, so we can use collection initializers
+        public IEnumerator<KeyValuePair<TEnum, TConvertedValue>> GetEnumerator()
+            => convertedValues.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => convertedValues.GetEnumerator();
+
+        public void Add(TEnum enumValue, TConvertedValue convertedValue)
+            => convertedValues.Add(enumValue, convertedValue);
+        #endregion
+
+    }
+
+}
---
+++ Helpers/EnumToStringConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace BToolbox.Helpers
+{
+    public class EnumToStringConverter<TEnum> : EnumConverter<TEnum, string>
+    {
+        public EnumToStringConverter(string defaultValue = null, Dictionary<TEnum, string> convertedValues = null)
+            : base(defaultValue, convertedValues) { }
+    }
+}
