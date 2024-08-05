ada98b0862563fae7f6cc226bb49be7f40511845
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-08-05 13:08:05 +0200

    AddOrReplaceValues for dictionaries

---
+++ Helpers/DictionaryHelpers.cs
@@ -0,0 +1,26 @@
+﻿namespace BToolbox.Helpers
+{
+    public static class DictionaryHelpers
+    {
+
+        public static TValue GetAnyway<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> createDelegate)
+        {
+            if (!dictionary.TryGetValue(key, out TValue value))
+            {
+                value = createDelegate(key);
+                dictionary.Add(key, value);
+            }
+            return value;
+        }
+
+        public static TValue GetAnyway<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key) where TValue : new()
+            => GetAnyway(dictionary, key, k => new TValue());
+
+        public static void AddOrReplaceValues<TKey, TValue>(this Dictionary<TKey, TValue> changed, Dictionary<TKey, TValue> replacements)
+        {
+            foreach (KeyValuePair<TKey, TValue> kvp in replacements)
+                changed[kvp.Key] = kvp.Value;
+        }
+
+    }
+}
