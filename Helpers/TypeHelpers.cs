99f8ffa678ca2805b9c86fa66e13d0f8301238ca
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-08-08 13:42:33 +0200

    Add IsNumericType

---
+++ Helpers/TypeHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToolbox.Helpers
+{
+    public static class TypeHelpers
+    {
+        // @source https://stackoverflow.com/a/1750024
+        public static bool IsNumericType(this object o)
+        {
+            switch (Type.GetTypeCode(o.GetType()))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
